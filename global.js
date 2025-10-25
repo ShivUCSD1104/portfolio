662deb4013d7319270d54fd94fc1fd9f48313d3c
ShivUCSD1104 <shm010@ucsd.edu>
2025-10-24 19:57:17 -0700

    added year to projs

--- global.js
+++ global.js
@@ -30,6 +30,7 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
     // Define the content dynamically using innerHTML
     article.innerHTML = `
       <${headingLevel}>${project.title}</${headingLevel}>
+      <div class="project-year">${project.year}</div>
       <img src="${project.image}" alt="${project.title}">
       <p>${project.description}</p>
     `;
